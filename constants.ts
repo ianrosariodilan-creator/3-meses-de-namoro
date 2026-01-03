11d6ef2e13f812ec92361a39a4c0be53a83ac450
ianrosariodilan-creator <ianrosariodilan@gmail.com>
2026-01-03 00:16:29 -0300

    as imagens ficavam roxas, então mudei index e constants

--- constants.ts
+++ constants.ts
@@ -9,19 +9,19 @@ export const quizData: QuizPhase[] = [
         type: 'standard',
         question: "Para os fracos, o namoro começou há 3 meses. Mas qual foi a data exata do nosso primeiro beijo (o marco zero)?",
         answer: "Exato! Eu disse 'Começou com o nosso beijo no uber' e tu, muito romântica, respondeste 'Só por fogo no kur' 🔥.",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.50 (2).jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.50 (2).jpeg',
       },
       {
         type: 'standard',
         question: "Onde estávamos no nosso primeiro encontro oficial?",
         answer: "Pátio Belém... Foi perfeito.",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.49.jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.49.jpeg',
       },
       {
         type: 'standard',
         question: "Teste de Visão: Qual era a cor da roupa que você estava usando nesse primeiro encontro (e que eu nunca esqueci)?",
         answer: "Rosa. Você estava linda (como sempre).",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.48 (3).jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.48 (3).jpeg',
       },
     ],
   },
@@ -32,25 +32,25 @@ export const quizData: QuizPhase[] = [
         type: 'standard',
         question: 'Apresentando meus melhores amigos "Lambinho" e "Chupinho"... Quem são as lendas?',
         answer: "Sem comentários. 🦇",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.48 (2).jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.48 (2).jpeg',
       },
       {
         type: 'standard',
         question: 'Se eu pudesse comer apenas uma coisa para o resto da vida, o prato seria:',
         answer: "Yakisoba é vida. Acertou!",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.48 (1).jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.48 (1).jpeg',
       },
       {
         type: 'standard',
         question: 'Qual é a minha série/anime de conforto (aquela que eu defendo com unhas e dentes e já vi mil vezes)?',
         answer: "Re:Zero!!! O melhor de todos. Sem discussão.",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.48.jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.48.jpeg',
       },
       {
         type: 'standard',
         question: 'O que me faz querer virar as costas e sair de uma loja imediatamente (me irrita muito)?',
         answer: "Odeio ter que perguntar 'quanto custa?'. Tem que ter o preço!",
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.50 (1).jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.50 (1).jpeg',
       },
       {
         type: 'multiple-choice',
@@ -60,7 +60,7 @@ export const quizData: QuizPhase[] = [
             { text: 'Ficar na cama até tarde (dorminhoco)' },
             { text: 'Acordar cedo', isCorrect: true },
         ],
-        image: '/media/WhatsApp Image 2026-01-02 at 21.29.50.jpeg',
+        image: 'media/WhatsApp Image 2026-01-02 at 21.29.50.jpeg',
       },
     ],
   },
@@ -71,13 +71,13 @@ export const quizData: QuizPhase[] = [
             type: 'standard',
             question: 'Qual é a música (peculiar) que toca e me faz lembrar de você na hora?',
             answer: "O jeito é dar uma fugidinha com voce ♪♪",
-            image: '/media/WhatsApp Image 2026-01-02 at 21.29.51 (1).jpeg'
+            image: 'media/WhatsApp Image 2026-01-02 at 21.29.51 (1).jpeg'
         },
         {
             type: 'standard',
             question: 'Qual foi a coisa mais engraçada (e estúpida) que já nos aconteceu juntos?',
             answer: 'Rindo agora, mas na hora foi o caos. Sobrevivemos!',
-            image: '/media/WhatsApp Image 2026-01-02 at 21.29.51.jpeg'
+            image: 'media/WhatsApp Image 2026-01-02 at 21.29.51.jpeg'
         }
     ]
   },
@@ -93,7 +93,7 @@ export const quizData: QuizPhase[] = [
             type: 'standard',
             question: 'Sinceramente, o que eu mais gosto no teu corpo?',
             answer: 'O seu rosto. Dentre todas as fotos, eu prefiro sempre aquela do seu rosto feliz.',
-            image: '/media/WhatsApp Image 2026-01-02 at 21.29.52.jpeg' // Mandatory image
+            image: 'media/WhatsApp Image 2026-01-02 at 21.29.52.jpeg' // Mandatory image
         }
     ]
   },
@@ -131,7 +131,7 @@ export const finalQuestionData: QuizQuestion = {
 };
 
 export const finalVideoData: VideoData = {
-    path: '/media/WhatsApp Video 2026-01-02 at 21.29.47.mp4',
+    path: 'media/WhatsApp Video 2026-01-02 at 21.29.47.mp4',
     message: 'Vou estar sempre com você, nos melhores e piores momentos.',
     finalWords: 'Feliz 3 meses, meu amor! Te amo. ❤️'
 };
